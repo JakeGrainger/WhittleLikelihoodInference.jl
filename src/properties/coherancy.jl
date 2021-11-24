cb77cedb6c2fc7f93779a2d043007741c3626532
Jake Grainger <j.p.grainger2@outlook.com>
2021-11-24 17:36:51 +0000

    Added docstrings for coherancy

--- src/properties/coherancy.jl
+++ src/properties/coherancy.jl
@@ -1,12 +1,25 @@
-function coherancy(model::TimeSeriesModel{D}, ω) where {D}
+"""
+    coherancy(model::TimeSeriesModel, ω)
+
+Compute the coherancy of a given model at frequency `ω`.
+"""
+function coherancy(model::TimeSeriesModel{D}, ω<:Number) where {D}
     S = sdf(model, ω)
     return [S[i,j]/sqrt(S[i,i]*S[j,j]) for i = 1:D, j = 1:D]
 end
-coherancy(model::TimeSeriesModel, Ω::AbstractVector{T}) where {T} = coherancy.(Ref(model), Ω)
 
+"""
+    coherance(model::TimeSeriesModel, ω)
+
+Compute the coherance of a given model at frequency `ω`.
+"""
 coherance(model::TimeSeriesModel, ω) = abs.(coherancy(model, ω))
-coherance(model::TimeSeriesModel, Ω::AbstractVector{T}) where {T} = coherance.(Ref(model), Ω)
 
+"""
+    groupdelay(model::TimeSeriesModel, ω)
+
+Compute the groupdelay of a given model at frequency `ω`.
+"""
 groupdelay(model::TimeSeriesModel, ω) = angle.(coherancy(model, ω))
-groupdelay(model::TimeSeriesModel, Ω::AbstractVector{T}) where {T} = groupdelay.(Ref(model), Ω)
+
 

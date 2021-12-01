d90788b78806cc8e664a641e020cd1fb5d8d1e69
Jake Grainger <j.p.grainger2@outlook.com>
2021-12-01 17:56:17 +0000

    Added tests for typestructure file.

---
+++ test/typestructure_test.jl
@@ -0,0 +1,42 @@
+import WhittleLikelihoodInference:  nlowertriangle_dimension, 
+                                    nlowertriangle_parameter,
+                                    indexLT, nalias, minbins
+const OUDouble = OU+OU
+@testset "typestructure" begin
+    @testset "AdditiveTimeSeriesModel" begin
+        @test AdditiveTimeSeriesModel(OU(1,1),OU(1,1)) == AdditiveTimeSeriesModel{OU,OU,1}(ones(4))
+        @test_throws MethodError AdditiveTimeSeriesModel(OU(1,1),CorrelatedOU(1,1,1/2))
+        @test OUDouble == AdditiveTimeSeriesModel{OU,OU,1}
+        @test_throws MethodError OU+CorrelatedOU
+    end
+    @testset "ndims" begin
+        @test ndims(OU) == 1
+        @test ndims(Matern{10,55}) == 10
+        @test ndims(CorrelatedOU(1,1,1/2)) == 2
+    end
+    @testset "nlowertriangle_dimension" begin
+        @test nlowertriangle_dimension(OU) == 1
+        @test nlowertriangle_dimension(Matern{10,55}) == 55
+        @test nlowertriangle_dimension(CorrelatedOU(1,1,1/2)) == 3
+    end
+    @testset "npars" begin
+        @test npars(OU) == 2
+        @test npars(CorrelatedOU(1,1,1/2)) == 3
+        @test npars(OUDouble) == 4
+    end
+    @testset "nlowertriangle_parameter" begin
+        @test nlowertriangle_parameter(OU) == 3
+        @test nlowertriangle_parameter(CorrelatedOU(1,1,1/2)) == 6
+        @test nlowertriangle_parameter(OUDouble) == 10
+    end
+    @testset "indexLT" begin
+        @test indexLT(2,3,3) == 5
+        @test indexLT(4,2,5) == 8
+    end
+    @testset "nalias" begin
+        @test nalias(OU(1,1)) isa Int
+    end
+    @testset "minbins" begin
+        @test minbins(OU(1,1)) isa Int
+    end
+end

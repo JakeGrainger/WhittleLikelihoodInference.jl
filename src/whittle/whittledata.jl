9be37e1f67dc1d70e2779d44b107576cb041b550
Jake Grainger <j.p.grainger2@outlook.com>
2021-12-08 13:57:34 +0000

    Added possible taper for Whittle data and refactored some code. Added extra tests.

--- src/whittle/whittledata.jl
+++ src/whittle/whittledata.jl
@@ -11,22 +11,10 @@ struct WhittleData{T} <: GenWhittleData
     n::Int64
     Δ::Float64
     Ω_used_index::Vector{Int64}
-    function WhittleData(::Type{<:TimeSeriesModel{D}}, timeseries::Matrix{S}, Δ; lowerΩcutoff = 0, upperΩcutoff = Inf) where {D,S<:Real}
-        size(timeseries, 2) == D || throw(ArgumentError("Time series has $(size(timeseries, 2)) dimensions, but model has $D dimension."))
-        n = size(timeseries, 1)
-        Ω = fftfreq(n, 2π/Δ)
-        Ω_used_index = (1:n)[lowerΩcutoff .< abs.(Ω) .< upperΩcutoff]
-        J = fft(timeseries, 1)
-        I = [SMatrix{D,D}(J[ii, :]*J[ii, :]'.*(Δ / (2π * n))) for ii = Ω_used_index]
-        new{eltype(I)}(I, n, Δ, Ω_used_index)
-    end
-    function WhittleData(::Type{<:TimeSeriesModel{1}}, timeseries::Vector{S}, Δ; lowerΩcutoff = 0, upperΩcutoff = Inf) where {S<:Real}
-        n = size(timeseries, 1)
-        Ω = fftfreq(n, 2π/Δ)
-        Ω_used_index = (1:n)[lowerΩcutoff .< abs.(Ω) .< upperΩcutoff]
-        J = fft(timeseries, 1)
-        I = abs.(J).^2 .* (Δ / (2π * n))
-        new{eltype(I)}(I[Ω_used_index], n, Δ, Ω_used_index)
+    function WhittleData(::Type{<:TimeSeriesModel{D}}, timeseries::Matrix{S}, Δ; lowerΩcutoff = 0, upperΩcutoff = Inf, taper = nothing) where {D,S<:Real}
+        checkwhittledatainputs(timeseries,taper,Val{D}())
+        ordinates,n,Ω_used_index = makewhittledata(timeseries, Δ, Val{D}())
+        new{eltype(ordinates)}(ordinates, n, Δ, Ω_used_index)
     end
 end
 
@@ -40,22 +28,35 @@ struct DebiasedWhittleData{T} <: GenWhittleData
     n::Int64
     Δ::Float64
     Ω_used_index::Vector{Int64}
-    function DebiasedWhittleData(::Type{<:TimeSeriesModel{D}}, timeseries::Matrix{S}, Δ; lowerΩcutoff = 0, upperΩcutoff = Inf) where {D,S<:Real}
-        size(timeseries, 2) == D || throw(ArgumentError("Time series has $(size(timeseries, 2)) dimensions, but model has $D dimension."))
-        n = size(timeseries, 1)
-        Ω = fftfreq(n, 2π/Δ)
-        Ω_used_index = (1:n)[lowerΩcutoff .< abs.(Ω) .< upperΩcutoff]
-        J = fft(timeseries, 1)
-        I = [SMatrix{D,D}(J[ii, :]*J[ii, :]'.*(Δ / (2π * n))) for ii = Ω_used_index]
-        new{eltype(I)}(I, n, Δ, 2 .* Ω_used_index .- 1) # accounts for double resolution in EI computations
-    end
-    function DebiasedWhittleData(::Type{<:TimeSeriesModel{1}}, timeseries::Vector{S}, Δ; lowerΩcutoff = 0, upperΩcutoff = Inf) where {S<:Real}
-        n = size(timeseries, 1)
-        Ω = fftfreq(n, 2π/Δ)
-        Ω_used_index = (1:n)[lowerΩcutoff .< abs.(Ω) .< upperΩcutoff]
-        J = fft(timeseries, 1)
-        I = abs.(J).^2 .* (Δ / (2π * n))
-        new{eltype(I)}(I[Ω_used_index], n, Δ, 2 .* Ω_used_index .- 1) # accounts for double resolution in EI computations
+    function DebiasedWhittleData(::Type{<:TimeSeriesModel{D}}, timeseries::Vector{S}, Δ; lowerΩcutoff = 0, upperΩcutoff = Inf, taper = nothing) where {S<:Real,D}
+        checkwhittledatainputs(timeseries,taper,Val{D}())
+        ordinates,n,Ω_used_index = makewhittledata(timeseries, Δ, Val{D}())
+        new{eltype(I)}(ordinates, n, Δ, 2 .* Ω_used_index .- 1) # accounts for double resolution in EI computations
     end
 end
 Base.show(io::IO, W::GenWhittleData) = print(io, "Precomputed periodogram for $(size(W.I[1],1)) dimensional timeseries of length $(W.n) with a sampling rate of $(W.Δ).")
+
+## internal for whittle data
+function checkwhittledatainputs(timeseries,taper,::Val{D}) where {D}
+    size(timeseries, 2) == D || throw(ArgumentError("Time series has $(size(timeseries, 2)) dimensions, but model has $D dimension."))
+    if !(taper === nothing)
+        taper isa AbstractVector{T} where {T<:Real} || throw(ArgumentError("Taper should be an AbstractVector of reals."))
+        length(taper) == size(timeseries,1) || throw(ArgumentError("taper should be same length as timeseries"))
+    end
+    nothing
+end
+
+function makewhittledata(timeseries, Δ, lowerΩcutoff, upperΩcutoff)
+    n = size(timeseries, 1)
+    Ω = fftfreq(n, 2π/Δ)
+    Ω_used_index = (1:n)[lowerΩcutoff .< abs.(Ω) .< upperΩcutoff]
+    return makeI(timeseries, Δ)[Ω_used_index], n, Ω_used_index
+end
+function makeI(timeseries::Matrix{T}, Δ, ::Val{D}) where {T,D}
+    J = fft(timeseries, 1)
+    return [SMatrix{D,D}(j*j'.*(Δ / (2π * n))) for j in eachrow(J)]
+end
+function makeI(timeseries::Vector{T}, Δ, ::Val{1}) where {T}
+    J = fft(timeseries, 1)
+    return abs.(J).^2 .* (Δ / (2π * n))
+end
--- test/whittle/whittledata_test.jl
+++ test/whittle/whittledata_test.jl
@@ -1,4 +1,8 @@
 @testset "whittledata" begin
     @test_throws ArgumentError WhittleLikelihoodInference.WhittleData(CorrelatedOU, ones(10,3), 1.0)
     @test_throws ArgumentError WhittleLikelihoodInference.DebiasedWhittleData(CorrelatedOU, ones(10,3), 1.0)
+    @test_throws ArgumentError WhittleLikelihoodInference.WhittleData(CorrelatedOU, ones(10,3), 1.0, taper = ones(10,2))
+    @test_throws ArgumentError WhittleLikelihoodInference.DebiasedWhittleData(CorrelatedOU, ones(10,3), 1.0, taper = ones(10,2))
+    @test_throws ArgumentError WhittleLikelihoodInference.WhittleData(CorrelatedOU, ones(10,3), 1.0, taper = ones(ComplexF64,10))
+    @test_throws ArgumentError WhittleLikelihoodInference.DebiasedWhittleData(CorrelatedOU, ones(10,3), 1.0, taper = ones(ComplexF64,10))
 end

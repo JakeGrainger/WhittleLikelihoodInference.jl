931b9eae4e3f69b4b61395dad0631d7a468486d2
Jake Grainger <j.p.grainger2@outlook.com>
2021-12-08 16:54:29 +0000

    Refactored expected periodogram code and added tapering.

--- src/properties/expectedperiodogram.jl
+++ src/properties/expectedperiodogram.jl
@@ -9,32 +9,73 @@ Compute the expected periodogram and assign to appropriate place in memory (note
 """
 function EI!(store::TimeSeriesModelStorageFunction, model::TimeSeriesModel)
     acv!(store, model)
-    _EI!(store)
+    _func_EI!(store)
+    return nothing
+end
+_func_EI!(store::AdditiveStorage) = _func_EI!(store.store1)
+_func_EI!(store::TimeSeriesModelStorageFunction) = _EI!(store.funcmemory, store.encodedtime.Δ)
+
+"""
+    grad_EI!(store::TimeSeriesModelStorageGradient, model::TimeSeriesModel)
+
+Compute the gradient of the expected periodogram and assign to appropriate place in memory (note this computes at twice the desired resolution).
+"""
+function grad_EI!(store::TimeSeriesModelStorageGradient, model::TimeSeriesModel)
+    grad_acv!(store, model)
+    _EI!(store.gradmemory, store.encodedtime.Δ)
+    return nothing
+end
+function grad_EI!(store::AdditiveStorage, model::AdditiveTimeSeriesModel)
+    grad_EI!(store.store1, model.model1)
+    grad_EI!(store.store2, model.model2)
     return nothing
 end
 
 """
-    _EI!(store, n, Δ)
+    hess_EI!(store::TimeSeriesModelStorageHessian, model::TimeSeriesModel)
 
-Interior function for `EI!`.
+Compute the Hessian of the expected periodogram and assign to appropriate place in memory (note this computes at twice the desired resolution).
 """
-_EI!(store::TimeSeriesModelStorageFunction) = _EI!(store.funcmemory, store.encodedtime.n, store.encodedtime.Δ)
-function _EI!(store::Sdf2EIStorage, n, Δ) # dispatch to alternate version to pull out correct storage
-    _EI!(store.acv2EI, n, Δ)
+function hess_EI!(store::TimeSeriesModelStorageHessian, model::TimeSeriesModel)
+    hess_acv!(store, model)
+    _hess_EI!(store.hessmemory, store.encodedtime.n, store.encodedtime.Δ)
     return nothing
 end
-function _EI!(store::Acv2EIStorage, n, Δ)
-    2n == size(store.allocatedarray,2) || throw(ArgumentError("store.allocatedarray should have second dimension size 2n."))
-    @inbounds for i ∈ 1:n
-        @views store.allocatedarray[:, i] .*= (1 - (i - 1) / n)
-    end
-    @inbounds for i ∈ n+1:size(store.allocatedarray, 2)
-        @views store.allocatedarray[:, i] .*= (1 - (2n - i + 1) / n)
-    end
-    store.planned_fft * store.allocatedarray # essentially fft!(acv, 2)
+function hess_EI!(store::AdditiveStorage, model::AdditiveTimeSeriesModel)
+    @views hess_EI!(store.store1, model.model1)
+    @views hess_EI!(store.store2, model.model2)
+    return nothing
+end
+
+_EI!(store::Sdf2EIStorage, Δ) = __EI!(store.acv2EI, Δ)
+_EI!(store::Acv2EIStorage, Δ) = __EI!(store, Δ)
+_EI!(store::Sdf2EIStorageUni, Δ) = __EI!(store.acv2EI, Δ)
+_EI!(store::Acv2EIStorageUni, Δ) = __EI!(store, Δ)
+
+function __EI!(store, Δ)
+    mult_allocated_kernel!(store.allocatedarray, store.kernel)
+    store.planned_fft * store.allocatedarray # essentially fft!(acv, ndims(store.allocatedarray))
     store.allocatedarray .*= Δ / (2π)
-    
     return nothing
+end
+
+function mult_allocated_kernel!(array::Vector{ComplexF64},kernel)
+    array .*= kernel
+    nothing
+end
+function mult_allocated_kernel!(array::Matrix{ComplexF64},kernel)
+    size(array, 2) == length(kernel) || throw(DimensionMismatch("size of second dimension of array should equal length of kernel."))
+    @inbounds for i ∈ 1:size(array, 2)
+        @views array[:, i] .*= kernel[i]
+    end
+    nothing
+end
+function mult_allocated_kernel!(array::Array{ComplexF64,3},kernel)
+    size(array, 3) == length(kernel) || throw(DimensionMismatch("size of third dimension of array should equal length of kernel."))
+    @inbounds for i ∈ 1:size(array, 3)
+        @views array[:, :, i] .*= kernel[i]
+    end
+    nothing
 end
 
 """
@@ -48,6 +89,8 @@ Extract the EI from general storage.
 extract_EI(store) = extract_EI(extract_S(store))
 extract_EI(store::Sdf2EIStorage) = extract_EI(store.acv2EI)
 extract_EI(store::Acv2EIStorage) = store.hermitianarray
+extract_EI(store::Sdf2EIStorageUni) = extract_EI(store.acv2EI)
+extract_EI(store::Acv2EIStorageUni) = store.allocatedarray
 
 """
     EI(model::TimeSeriesModel, n, Δ)
@@ -60,169 +103,4 @@ function EI(model::TimeSeriesModel, n, Δ)
     store = allocate_memory_EI_F(typeof(model), n , Δ)
     EI!(store, model)
     return fftshift(copy(extract_EI(store)))[1:2:end]
-end
-
-### Univariate ###
-
-function _EI!(store::Sdf2EIStorageUni, n, Δ) # dispatch to alternate version to pull out correct storage
-    _EI!(store.acv2EI, n, Δ)
-    return nothing
-end
-function _EI!(store::Acv2EIStorageUni, n, Δ)
-    2n == length(store.allocatedarray) || throw(ArgumentError("store.allocatedarray should have length 2n."))
-    @inbounds for i ∈ 1:n
-        @views store.allocatedarray[i] *= (1 - (i - 1) / n)
-    end
-    @inbounds for i ∈ n+1:size(store.allocatedarray, 2)
-        @views store.allocatedarray[i] *= (1 - (2n - i + 1) / n)
-    end
-    store.planned_fft * store.allocatedarray # essentially fft!(acv, 1)
-    store.allocatedarray .*= Δ / (2π)
-    
-    return nothing
-end
-
-extract_EI(store::Sdf2EIStorageUni) = extract_EI(store.acv2EI)
-extract_EI(store::Acv2EIStorageUni) = store.allocatedarray
-
-### Additive ###
-function EI!(store::AdditiveStorage, model::AdditiveTimeSeriesModel)
-    acv!(store, model)
-    _EI!(store.store1)
-end
-_EI!(store::AdditiveStorage) = _EI!(store.store1) # store could contain additive store so need this for recursion.
-
-
-
-## Gradient of expected periodogram ##
-
-### Multivariate ###
-
-"""
-    grad_EI!(store::TimeSeriesModelStorageGradient, model::TimeSeriesModel)
-
-Compute the gradient of the expected periodogram and assign to appropriate place in memory (note this computes at twice the desired resolution).
-"""
-function grad_EI!(store::TimeSeriesModelStorageGradient, model::TimeSeriesModel)
-    grad_acv!(store, model)
-    _grad_EI!(store.gradmemory, store.encodedtime.n, store.encodedtime.Δ)
-    return nothing
-end
-
-"""
-    _grad_EI!(store, n, Δ)
-
-Interior function for `grad_EI`.
-"""
-function _grad_EI!(store::Sdf2EIStorage, n, Δ) # dispatch to alternate version to pull out correct storage
-    _grad_EI!(store.acv2EI, n, Δ)
-    return nothing
-end
-function _grad_EI!(store::Acv2EIStorage, n, Δ)
-    2n == size(store.allocatedarray,3) || throw(ArgumentError("store.allocatedarray should have third dimension size 2n."))
-    @inbounds for i ∈ 1:n
-        @views store.allocatedarray[:, :, i] .*= (1 - (i - 1) / n)
-    end
-    @inbounds for i ∈ n+1:size(store.allocatedarray, 3)
-        @views store.allocatedarray[:, :, i] .*= (1 - (2n - i + 1) / n)
-    end
-    store.planned_fft * store.allocatedarray # essentially fft!(acv, 2)
-    store.allocatedarray .*= Δ / (2π)
-    
-    return nothing
-end
-
-### Univariate ###
-
-function _grad_EI!(store::Sdf2EIStorageUni, n, Δ) # dispatch to alternate version to pull out correct storage
-    _grad_EI!(store.acv2EI, n, Δ)
-    return nothing
-end
-function _grad_EI!(store::Acv2EIStorageUni, n, Δ)
-    2n == size(store.allocatedarray,3) || throw(ArgumentError("store.allocatedarray should have second dimension size 2n."))
-    @inbounds for i ∈ 1:n
-        @views store.allocatedarray[:, i] .*= (1 - (i - 1) / n)
-    end
-    @inbounds for i ∈ n+1:size(store.allocatedarray, 2)
-        @views store.allocatedarray[:, i] .*= (1 - (2n - i + 1) / n)
-    end
-    store.planned_fft * store.allocatedarray # essentially fft!(acv, 2)
-    store.allocatedarray .*= Δ / (2π)
-    
-    return nothing
-end
-
-### Additive ###
-
-function grad_EI!(store::AdditiveStorage, model::AdditiveTimeSeriesModel)
-    @views grad_EI!(store.store1, model.model1)
-    @views grad_EI!(store.store2, model.model2)
-    return nothing
-end
-
-
-## Hessian of expected periodogram ##
-
-### Multivariate ###
-
-"""
-    hess_EI!(store::TimeSeriesModelStorageHessian, model::TimeSeriesModel)
-
-Compute the Hessian of the expected periodogram and assign to appropriate place in memory (note this computes at twice the desired resolution).
-"""
-function hess_EI!(store::TimeSeriesModelStorageHessian, model::TimeSeriesModel)
-    hess_acv!(store, model)
-    _hess_EI!(store.hessmemory, store.encodedtime.n, store.encodedtime.Δ)
-    return nothing
-end
-
-"""
-    _hess_EI!(store, n, Δ)
-
-Interior function for `hess_EI`.
-"""
-function _hess_EI!(store::Sdf2EIStorage, n, Δ) # dispatch to alternate version to pull out correct storage
-    _hess_EI!(store.acv2EI, n, Δ)
-    return nothing
-end
-function _hess_EI!(store::Acv2EIStorage, n, Δ)
-    2n == size(store.allocatedarray,3) || throw(ArgumentError("store.allocatedarray should have third dimension size 2n."))
-    @inbounds for i ∈ 1:n
-        @views store.allocatedarray[:, :, i] .*= (1 - (i - 1) / n)
-    end
-    @inbounds for i ∈ n+1:size(store.allocatedarray, 3)
-        @views store.allocatedarray[:, :, i] .*= (1 - (2n - i + 1) / n)
-    end
-    store.planned_fft * store.allocatedarray # essentially fft!(acv, 2)
-    store.allocatedarray .*= Δ / (2π)
-    
-    return nothing
-end
-
-### Univariate ###
-
-function _hess_EI!(store::Sdf2EIStorageUni, n, Δ) # dispatch to alternate version to pull out correct storage
-    _hess_EI!(store.acv2EI, n, Δ)
-    return nothing
-end
-function _hess_EI!(store::Acv2EIStorageUni, n, Δ)
-    2n == size(store.allocatedarray,2) || throw(ArgumentError("store.allocatedarray should have second dimension size 2n."))
-    @inbounds for i ∈ 1:n
-        @views store.allocatedarray[:, i] .*= (1 - (i - 1) / n)
-    end
-    @inbounds for i ∈ n+1:size(store.allocatedarray, 2)
-        @views store.allocatedarray[:, i] .*= (1 - (2n - i + 1) / n)
-    end
-    store.planned_fft * store.allocatedarray # essentially fft!(acv, 2)
-    store.allocatedarray .*= Δ / (2π)
-    
-    return nothing
-end
-
-### Additive ###
-
-function hess_EI!(store::AdditiveStorage, model::AdditiveTimeSeriesModel)
-    @views hess_EI!(store.store1, model.model1)
-    @views hess_EI!(store.store2, model.model2)
-    return nothing
 end
--- test/properties/expectedperiodogram_test.jl
+++ test/properties/expectedperiodogram_test.jl
@@ -1,17 +1,18 @@
 @testset "expectedperiodogram" begin
     store = allocate_memory_EI_FGH(CorrelatedOU,100,1)
     storeuni = allocate_memory_EI_FGH(OU,100,1)
+    badkernel = ones(50) # kernel should be length 200
     @testset "Function" begin
-        @test_throws ArgumentError WhittleLikelihoodInference._EI!(store.funcmemory,200,1)
-        @test_throws ArgumentError WhittleLikelihoodInference._EI!(storeuni.funcmemory,200,1)
+        @test_throws DimensionMismatch WhittleLikelihoodInference.mult_allocated_kernel!(store.funcmemory.allocatedarray,badkernel)
+        @test_throws DimensionMismatch WhittleLikelihoodInference.mult_allocated_kernel!(storeuni.funcmemory.allocatedarray,badkernel)
         @test length(EI(OU(1.0,1.0),10,1/2)) == 10
     end
     @testset "Gradient" begin
-        @test_throws ArgumentError WhittleLikelihoodInference._grad_EI!(store.gradmemory,200,1)
-        @test_throws ArgumentError WhittleLikelihoodInference._grad_EI!(storeuni.gradmemory,200,1)
+        @test_throws DimensionMismatch WhittleLikelihoodInference.mult_allocated_kernel!(store.gradmemory.allocatedarray,badkernel)
+        @test_throws DimensionMismatch WhittleLikelihoodInference.mult_allocated_kernel!(storeuni.gradmemory.allocatedarray,badkernel)
     end
     @testset "Hessian" begin
-        @test_throws ArgumentError WhittleLikelihoodInference._hess_EI!(store.hessmemory,200,1)
-        @test_throws ArgumentError WhittleLikelihoodInference._hess_EI!(storeuni.hessmemory,200,1)
+        @test_throws DimensionMismatch WhittleLikelihoodInference.mult_allocated_kernel!(store.hessmemory.allocatedarray,badkernel)
+        @test_throws DimensionMismatch WhittleLikelihoodInference.mult_allocated_kernel!(storeuni.hessmemory.allocatedarray,badkernel)
     end
 end

92d314bc7c692823587c68b3fb6a008a02d89ed8
Jake Grainger <j.p.grainger2@outlook.com>
2021-12-08 14:18:46 +0000

    Adjusted changes to whittledata file.

--- src/whittle/whittledata.jl
+++ src/whittle/whittledata.jl
@@ -2,7 +2,7 @@
 abstract type GenWhittleData end
 
 """
-    WhittleData(model::Type{<:TimeSeriesModel{D}}, timeseries, Δ; lowerΩcutoff, upperΩcutoff)
+    WhittleData(model::Type{<:TimeSeriesModel{D}}, timeseries, Δ; lowerΩcutoff, upperΩcutoff, taper)
 
 Create storage for timeseries data in a format which is useful for Whittle methods.
 """
@@ -11,15 +11,15 @@ struct WhittleData{T} <: GenWhittleData
     n::Int64
     Δ::Float64
     Ω_used_index::Vector{Int64}
-    function WhittleData(::Type{<:TimeSeriesModel{D}}, timeseries::Matrix{S}, Δ; lowerΩcutoff = 0, upperΩcutoff = Inf, taper = nothing) where {D,S<:Real}
-        checkwhittledatainputs(timeseries,taper,Val{D}())
-        ordinates,n,Ω_used_index = makewhittledata(timeseries, Δ, Val{D}())
+    function WhittleData(model::Type{<:TimeSeriesModel{D}}, timeseries::Array{T,N}, Δ; lowerΩcutoff = 0, upperΩcutoff = Inf, taper = nothing) where {T<:Real,D,N}
+        checkwhittledatainputs(model,timeseries,taper)
+        ordinates,n,Ω_used_index = makewhittledata(model, timeseries, Δ, lowerΩcutoff, upperΩcutoff, taper)
         new{eltype(ordinates)}(ordinates, n, Δ, Ω_used_index)
     end
 end
 
 """
-    DebiasedWhittleData(model::Type{<:TimeSeriesModel{D}}, timeseries, Δ; lowerΩcutoff, upperΩcutoff)
+    DebiasedWhittleData(model::Type{<:TimeSeriesModel{D}}, timeseries, Δ; lowerΩcutoff, upperΩcutoff, taper)
 
 Create storage for timeseries data in a format which is useful for debiased Whittle methods.
 """
@@ -28,16 +28,16 @@ struct DebiasedWhittleData{T} <: GenWhittleData
     n::Int64
     Δ::Float64
     Ω_used_index::Vector{Int64}
-    function DebiasedWhittleData(::Type{<:TimeSeriesModel{D}}, timeseries::Vector{S}, Δ; lowerΩcutoff = 0, upperΩcutoff = Inf, taper = nothing) where {S<:Real,D}
-        checkwhittledatainputs(timeseries,taper,Val{D}())
-        ordinates,n,Ω_used_index = makewhittledata(timeseries, Δ, Val{D}())
+    function DebiasedWhittleData(model::Type{<:TimeSeriesModel{D}}, timeseries::Array{T,N}, Δ; lowerΩcutoff = 0, upperΩcutoff = Inf, taper = nothing) where {T<:Real,D,N}
+        checkwhittledatainputs(model,timeseries,taper)
+        ordinates,n,Ω_used_index = makewhittledata(model, timeseries, Δ, lowerΩcutoff, upperΩcutoff, taper)
         new{eltype(I)}(ordinates, n, Δ, 2 .* Ω_used_index .- 1) # accounts for double resolution in EI computations
     end
 end
 Base.show(io::IO, W::GenWhittleData) = print(io, "Precomputed periodogram for $(size(W.I[1],1)) dimensional timeseries of length $(W.n) with a sampling rate of $(W.Δ).")
 
 ## internal for whittle data
-function checkwhittledatainputs(timeseries,taper,::Val{D}) where {D}
+function checkwhittledatainputs(::Type{<:TimeSeriesModel{D}},timeseries,taper) where {D}
     size(timeseries, 2) == D || throw(ArgumentError("Time series has $(size(timeseries, 2)) dimensions, but model has $D dimension."))
     if !(taper === nothing)
         taper isa AbstractVector{T} where {T<:Real} || throw(ArgumentError("Taper should be an AbstractVector of reals."))
@@ -46,17 +46,22 @@ function checkwhittledatainputs(timeseries,taper,::Val{D}) where {D}
     nothing
 end
 
-function makewhittledata(timeseries, Δ, lowerΩcutoff, upperΩcutoff)
+function makewhittledata(model, timeseries, Δ, lowerΩcutoff, upperΩcutoff, taper)
     n = size(timeseries, 1)
     Ω = fftfreq(n, 2π/Δ)
     Ω_used_index = (1:n)[lowerΩcutoff .< abs.(Ω) .< upperΩcutoff]
-    return makeI(timeseries, Δ)[Ω_used_index], n, Ω_used_index
+    if taper === nothing
+        tapered_timeseries = timeseries
+    else
+        tapered_timeseries = timeseries .* taper .* sqrt(n) # * √n because we divide by n in what follows, but shouldn't for tapering
+    end
+    return makeI(model, tapered_timeseries, n, Δ)[Ω_used_index], n, Ω_used_index
 end
-function makeI(timeseries::Matrix{T}, Δ, ::Val{D}) where {T,D}
+function makeI(::Type{<:TimeSeriesModel{D}},timeseries::Matrix{T}, n, Δ) where {T,D}
     J = fft(timeseries, 1)
     return [SMatrix{D,D}(j*j'.*(Δ / (2π * n))) for j in eachrow(J)]
 end
-function makeI(timeseries::Vector{T}, Δ, ::Val{1}) where {T}
+function makeI(::Type{<:TimeSeriesModel{1}},timeseries::Vector{T}, n, Δ) where {T}
     J = fft(timeseries, 1)
     return abs.(J).^2 .* (Δ / (2π * n))
 end

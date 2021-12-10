6c1e5f0ca3eca25d6fb799cb1172ccd05d134fbe
Jake Grainger <j.p.grainger2@outlook.com>
2021-12-10 23:22:43 +0000

    Changed name of MaternSlow to MaternAcv.

--- src/models/MaternAcv.jl
+++ src/models/MaternAcv.jl
@@ -1,4 +1,4 @@
-struct MaternSlow{D,L} <: TimeSeriesModel{D}
+struct MaternAcv{D,L} <: TimeSeriesModel{D}
     σ::SHermitianCompact{D,Float64,L}
     ν::SHermitianCompact{D,Float64,L}
     a::SHermitianCompact{D,Float64,L}
@@ -7,10 +7,10 @@ struct MaternSlow{D,L} <: TimeSeriesModel{D}
     variance_part::SHermitianCompact{D,Float64,L}
     sdfconst::SHermitianCompact{D,Float64,L}
     acvconst::SHermitianCompact{D,Float64,L}
-    function MaternSlow{D,L}(θ) where {D,L}
-        length(θ) == npars(Matern{D,L}) || throw(ArgumentError("MaternSlow process has $(npars(MaternSlow{D,L})) parameters, but $(length(θ)) were provided."))
-        L == triangularnumber(D) || error("MaternSlow{D,L} should satisfy L == D*(D+1)÷2")
-        all(x->x>0, θ) || throw(ArgumentError("all parameters of MaternSlow should be > 0."))
+    function MaternAcv{D,L}(θ) where {D,L}
+        length(θ) == npars(Matern{D,L}) || throw(ArgumentError("MaternAcv process has $(npars(MaternAcv{D,L})) parameters, but $(length(θ)) were provided."))
+        L == triangularnumber(D) || error("MaternAcv{D,L} should satisfy L == D*(D+1)÷2")
+        all(x->x>0, θ) || throw(ArgumentError("all parameters of MaternAcv should be > 0."))
         σ = @views SHermitianCompact(SVector{L,Float64}(θ[1:L]))
         all(i==j ? true : σ[i,j] < 1 for i in 1:size(σ,1) for j in 1:i) || throw(ArgumentError("ρ parameters must be < 1."))
         σ = @views SHermitianCompact(SVector{L,Float64}(θ[1:L]))
@@ -26,11 +26,11 @@ struct MaternSlow{D,L} <: TimeSeriesModel{D}
 end
 matern_acv_normalising(ν) = 2^(1-ν) / gamma(ν)
 
-npars(::Type{MaternSlow{D,L}}) where {D,L} = 3triangularnumber(D)
+npars(::Type{MaternAcv{D,L}}) where {D,L} = 3triangularnumber(D)
 
-parameternames(::Type{MaternSlow{D,L}}) where {D,L} = parameternames(Matern{D,L})
+parameternames(::Type{MaternAcv{D,L}}) where {D,L} = parameternames(Matern{D,L})
 
-function add_sdf!(out, model::MaternSlow{D,L}, ω) where {D,L}
+function add_sdf!(out, model::MaternAcv{D,L}, ω) where {D,L}
     ω² = ω^2
     count = 1
     for i ∈ 1:D, j ∈ i:D
@@ -40,7 +40,7 @@ function add_sdf!(out, model::MaternSlow{D,L}, ω) where {D,L}
     return nothing
 end
 
-function acv!(out, model::MaternSlow{D,L}, τ::Number) where {D,L}
+function acv!(out, model::MaternAcv{D,L}, τ::Number) where {D,L}
     modτ = abs(τ)
     count = 1
     for i ∈ 1:D, j ∈ i:D
@@ -51,7 +51,7 @@ function acv!(out, model::MaternSlow{D,L}, τ::Number) where {D,L}
     return nothing
 end
 
-struct MaternSlow1D <: TimeSeriesModel{1}
+struct MaternAcv1D <: TimeSeriesModel{1}
     σ::Float64
     ν::Float64
     a::Float64
@@ -61,22 +61,22 @@ struct MaternSlow1D <: TimeSeriesModel{1}
     variance_part::Float64
     sdfconst::Float64
     acvconst::Float64
-    function MaternSlow1D(σ,ν,a)
-        σ > 0 || throw(ArgumentError("MaternSlow1D process requires 0 < σ."))
-        ν > 0 || throw(ArgumentError("MaternSlow1D process requires 0 < ν."))
-        a > 0 || throw(ArgumentError("MaternSlow1D process requires 0 < a."))
+    function MaternAcv1D(σ,ν,a)
+        σ > 0 || throw(ArgumentError("MaternAcv1D process requires 0 < σ."))
+        ν > 0 || throw(ArgumentError("MaternAcv1D process requires 0 < ν."))
+        a > 0 || throw(ArgumentError("MaternAcv1D process requires 0 < a."))
         sdfconst = matern_sdf_normalising(ν,a)*σ^2
         acvconst = matern_acv_normalising(ν)*σ^2
         new(σ, ν, a, σ^2, a^2, ν+0.5, sdfconst, acvconst)
     end
-    function MaternSlow1D(x::AbstractVector{Float64})
-        length(x) == npars(MaternSlow1D) || throw(ArgumentError("MaternSlow1D process has $(npars(Matern1D)) parameters, but $(length(x)) were provided."))
+    function MaternAcv1D(x::AbstractVector{Float64})
+        length(x) == npars(MaternAcv1D) || throw(ArgumentError("MaternAcv1D process has $(npars(Matern1D)) parameters, but $(length(x)) were provided."))
         @inbounds Matern1D(x[1], x[2], x[3])
     end
 end
 
-npars(::Type{MaternSlow1D}) = 3
+npars(::Type{MaternAcv1D}) = 3
 
-sdf(model::MaternSlow1D, ω) = model.sdfconst/((model.a²+ω^2)^(model.νplushalf))
+sdf(model::MaternAcv1D, ω) = model.sdfconst/((model.a²+ω^2)^(model.νplushalf))
 
-acv(model::MaternSlow1D, τ) = abs(τ)>1e-10 ? model.acvconst * (amodτ)^model.ν * besselk(model.ν,a*abs(τ)) : model.σ²
+acv(model::MaternAcv1D, τ) = abs(τ)>1e-10 ? model.acvconst * (amodτ)^model.ν * besselk(model.ν,a*abs(τ)) : model.σ²

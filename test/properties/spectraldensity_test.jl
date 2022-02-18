62538ac5172537ac2a1ca3c359de1b5c426d770b
Jake Grainger <j.p.grainger2@outlook.com>
2022-02-18 18:37:25 +0000

    Add tests for real vs complex versions of asdf.

--- test/properties/spectraldensity_test.jl
+++ test/properties/spectraldensity_test.jl
@@ -1,7 +1,9 @@
 struct TestModel <: WhittleLikelihoodInference.TimeSeriesModel{2,Float64} end
 struct TestModelUni <: WhittleLikelihoodInference.TimeSeriesModel{1,Float64} end
 struct TestModel2 <: WhittleLikelihoodInference.TimeSeriesModel{2,Float64} end
+struct TestModel2C <: WhittleLikelihoodInference.TimeSeriesModel{2,ComplexF64} end
 struct TestModelUni2 <: WhittleLikelihoodInference.TimeSeriesModel{1,Float64} end
+struct TestModelUni2C <: WhittleLikelihoodInference.TimeSeriesModel{1,ComplexF64} end
 struct TestModel3 <: WhittleLikelihoodInference.TimeSeriesModel{2,Float64} end
 struct TestModelUni3 <: WhittleLikelihoodInference.TimeSeriesModel{1,Float64} end
 TestModel2(x) = TestModel2() # just to allow additive constructor to work
@@ -14,20 +16,20 @@ WhittleLikelihoodInference.npars(::Type{TestModel3}) = 4
 WhittleLikelihoodInference.npars(::Type{TestModelUni3}) = 4
 const TestDouble = TestModel2 + TestModel3
 const TestDoubleUni = TestModelUni2 + TestModelUni3
-WhittleLikelihoodInference.add_sdf!(out,::TestModel2,ω) = (out .+= 0.5; nothing)
-WhittleLikelihoodInference.grad_add_sdf!(out,::TestModel2,ω) = (out .+= 0.5; nothing)
-WhittleLikelihoodInference.hess_add_sdf!(out,::TestModel2,ω) = (out .+= 0.5; nothing)
-WhittleLikelihoodInference.sdf(::TestModelUni2,ω) = 0.5
-WhittleLikelihoodInference.grad_add_sdf!(out,::TestModelUni2,ω) = (out .+= 0.5; nothing)
-WhittleLikelihoodInference.hess_add_sdf!(out,::TestModelUni2,ω) = (out .+= 0.5; nothing)
+WhittleLikelihoodInference.add_sdf!(out,::Union{TestModel2,TestModel2C},ω) = (out .+= 0.5; nothing)
+WhittleLikelihoodInference.grad_add_sdf!(out,::Union{TestModel2,TestModel2C},ω) = (out .+= 0.5; nothing)
+WhittleLikelihoodInference.hess_add_sdf!(out,::Union{TestModel2,TestModel2C},ω) = (out .+= 0.5; nothing)
+WhittleLikelihoodInference.sdf(::Union{TestModelUni2,TestModelUni2C},ω) = 0.5
+WhittleLikelihoodInference.grad_add_sdf!(out,::Union{TestModelUni2,TestModelUni2C},ω) = (out .+= 0.5; nothing)
+WhittleLikelihoodInference.hess_add_sdf!(out,::Union{TestModelUni2,TestModelUni2C},ω) = (out .+= 0.5; nothing)
 WhittleLikelihoodInference.add_sdf!(out,::TestModel3,ω) = (out .+= 0.2; nothing)
 WhittleLikelihoodInference.grad_add_sdf!(out,::TestModel3,ω) = (out .+= 0.2; nothing)
 WhittleLikelihoodInference.hess_add_sdf!(out,::TestModel3,ω) = (out .+= 0.2; nothing)
 WhittleLikelihoodInference.sdf(::TestModelUni3,ω) = 0.2
 WhittleLikelihoodInference.grad_add_sdf!(out,::TestModelUni3,ω) = (out .+= 0.2; nothing)
 WhittleLikelihoodInference.hess_add_sdf!(out,::TestModelUni3,ω) = (out .+= 0.2; nothing)
-WhittleLikelihoodInference.nalias(::TestModel2) = 2
-WhittleLikelihoodInference.nalias(::TestModelUni2) = 2
+WhittleLikelihoodInference.nalias(::Union{TestModel2,TestModel2C}) = 2
+WhittleLikelihoodInference.nalias(::Union{TestModelUni2,TestModelUni2C}) = 2
 WhittleLikelihoodInference.nalias(::TestModel3) = 1
 WhittleLikelihoodInference.nalias(::TestModelUni3) = 1
 @testset "Spectral density" begin
@@ -55,6 +57,20 @@ WhittleLikelihoodInference.nalias(::TestModelUni3) = 1
             @test_throws ArgumentError WhittleLikelihoodInference.asdf!(store.funcmemory,TestModel2(),freqbad)
             @test_throws ArgumentError WhittleLikelihoodInference.asdf!(storeuni.funcmemory,TestModelUni2(),freqbad)
         end
+        @testset "Complex vs real" begin
+            for n in [100, 101]
+                store = allocate_memory_EI_F(TestModel2, n, 1.2)
+                store_c = allocate_memory_EI_F(TestModel2C, n, 1.2)
+                storeuni = allocate_memory_EI_F(TestModelUni2, n, 1.2)
+                storeuni_c = allocate_memory_EI_F(TestModelUni2C, n, 1.2)
+                WhittleLikelihoodInference.asdf!(store,TestModel2())
+                WhittleLikelihoodInference.asdf!(store_c,TestModel2C())
+                WhittleLikelihoodInference.asdf!(storeuni,TestModelUni2())
+                WhittleLikelihoodInference.asdf!(storeuni_c,TestModelUni2C())
+                @test store.allocatedarray == store_c.funcmemory.allocatedarray
+                @test storeuni.allocatedarray == storeuni_c.funcmemory.allocatedarray
+            end
+        end
         @testset "extract_asdf" begin
             @test_throws MethodError extract_asdf(allocate_memory_EI_F(TestModel2, 1000, 1).funcmemory)
             @test extract_asdf(allocate_memory_sdf_F(TestModel2, 1000, 1).funcmemory) isa Matrix{ComplexF64}
@@ -78,6 +94,20 @@ WhittleLikelihoodInference.nalias(::TestModelUni3) = 1
             @test_throws ArgumentError WhittleLikelihoodInference.grad_asdf!(store.gradmemory,TestModel2(),freqbad)
             @test_throws ArgumentError WhittleLikelihoodInference.grad_asdf!(storeuni.gradmemory,TestModelUni2(),freqbad)
         end
+        @testset "Complex vs real" begin
+            for n in [100, 101]
+                store = allocate_memory_EI_FG(TestModel2, n, 1.2)
+                store_c = allocate_memory_EI_FG(TestModel2C, n, 1.2)
+                storeuni = allocate_memory_EI_FG(TestModelUni2, n, 1.2)
+                storeuni_c = allocate_memory_EI_FG(TestModelUni2C, n, 1.2)
+                WhittleLikelihoodInference.grad_asdf!(store,TestModel2())
+                WhittleLikelihoodInference.grad_asdf!(store_c,TestModel2C())
+                WhittleLikelihoodInference.grad_asdf!(storeuni,TestModelUni2())
+                WhittleLikelihoodInference.grad_asdf!(storeuni_c,TestModelUni2C())
+                @test store.allocatedarray == store_c.gradmemory.allocatedarray
+                @test storeuni.allocatedarray == storeuni_c.gradmemory.allocatedarray
+            end
+        end
     end
     @testset "Hessian" begin
         @testset "Error handling" begin
@@ -96,6 +126,20 @@ WhittleLikelihoodInference.nalias(::TestModelUni3) = 1
             freqbad = FreqAcvEst(10,1)
             @test_throws ArgumentError WhittleLikelihoodInference.hess_asdf!(store.hessmemory,TestModel2(),freqbad)
             @test_throws ArgumentError WhittleLikelihoodInference.hess_asdf!(storeuni.hessmemory,TestModelUni2(),freqbad)
+        end
+        @testset "Complex vs real" begin
+            for n in [100, 101]
+                store = allocate_memory_EI_FGH(TestModel2, n, 1.2)
+                store_c = allocate_memory_EI_FGH(TestModel2C, n, 1.2)
+                storeuni = allocate_memory_EI_FGH(TestModelUni2, n, 1.2)
+                storeuni_c = allocate_memory_EI_FGH(TestModelUni2C, n, 1.2)
+                WhittleLikelihoodInference.hess_asdf!(store,TestModel2())
+                WhittleLikelihoodInference.hess_asdf!(store_c,TestModel2C())
+                WhittleLikelihoodInference.hess_asdf!(storeuni,TestModelUni2())
+                WhittleLikelihoodInference.hess_asdf!(storeuni_c,TestModelUni2C())
+                @test store.allocatedarray == store_c.hessmemory.allocatedarray
+                @test storeuni.allocatedarray == storeuni_c.hessmemory.allocatedarray
+            end
         end
     end
 end

fb60f96de159e3a5d91340db5b0f5c2ed7271345
Jake Grainger <j.p.grainger2@outlook.com>
2022-02-20 16:36:02 +0000

    Fix bug in expected periodogram hessian.

--- src/properties/expectedperiodogram.jl
+++ src/properties/expectedperiodogram.jl
@@ -38,7 +38,7 @@ Compute the Hessian of the expected periodogram and assign to appropriate place 
 """
 function hess_EI!(store::TimeSeriesModelStorageHessian, model::TimeSeriesModel)
     hess_acv!(store, model)
-    _EI!(store.hessmemory, store.encodedtime.n, store.encodedtime.Δ)
+    _EI!(store.hessmemory, store.encodedtime.Δ)
     return nothing
 end
 function hess_EI!(store::AdditiveStorage, model::AdditiveTimeSeriesModel)
